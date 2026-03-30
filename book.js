4ac9b89269ece847b6586ac4ab25998cfcda9050
alcxyz <me@alc.no>
2026-03-30 15:42:40 +0200

    cleanup

--- book.js
+++
@@ -1,3 +0,0 @@
-<!-- Elfsight Appointment Booking | annae.tattoo Appointment Booking -->
-<script src="https://elfsightcdn.com/platform.js" async></script>
-<div class="elfsight-app-7175524f-45be-48d7-8006-82ec694c134d" data-elfsight-app-lazy></div>
